d940c7df8fbd1d6f64eeb9c3817e999bf80677a1
dave <justdave@linux.com>
2021-12-30 15:28:37 -0600

    updated tests

--- ch_01/sha1_cracker/tests/services.rs
+++ ch_01/sha1_cracker/tests/services.rs
@@ -26,4 +26,25 @@ test_suite! {
         cmd.assert().success();
         cmd.assert().stdout(predicate::str::contains("Password found: "));
     }
+    test password_does_not_exist1() {
+        let mut cmd = Command::cargo_bin("sha1_cracker").unwrap();
+        cmd.arg("./wordlist.txt");
+        cmd.arg("a4ff81efb334996b21e9dc8229c5eb01bb226701");
+        cmd.assert().success();
+        cmd.assert().stdout(predicate::str::contains("password not found in wordlist :("));
+    }
+    test password_does_not_exist2() {
+        let mut cmd = Command::cargo_bin("sha1_cracker").unwrap();
+        cmd.arg("./wordlist.txt");
+        cmd.arg("ef89a3a842b0384565a210f0122804f411fe51fa");
+        cmd.assert().success();
+        cmd.assert().stdout(predicate::str::contains("password not found in wordlist :("));
+    }
+    test password_does_not_exist3() {
+        let mut cmd = Command::cargo_bin("sha1_cracker").unwrap();
+        cmd.arg("./wordlist.txt");
+        cmd.arg("ec461b5480380ecf863d9802edbe70152aee1c47");
+        cmd.assert().success();
+        cmd.assert().stdout(predicate::str::contains("password not found in wordlist :("));
+    }
 }
