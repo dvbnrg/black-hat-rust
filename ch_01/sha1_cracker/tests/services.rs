4dcae527b77b5b0856fdea76886b620990d5e922
dave <justdave@linux.com>
2021-12-29 22:02:18 -0600

    updated test

--- ch_01/sha1_cracker/tests/services.rs
+++ ch_01/sha1_cracker/tests/services.rs
@@ -5,11 +5,11 @@ test_suite! {
     use assert_cmd::prelude::*;
     use predicates::prelude::*;
     use std::process::Command;
-    test sha_cracker_trace_test1() {
+    test password_exists_in_list() {
         let mut cmd = Command::cargo_bin("sha1_cracker").unwrap();
         cmd.arg("./wordlist.txt");
         cmd.arg("a4ff81efb344996b21e9dc8229c5eb01bb226700");
         cmd.assert().success();
-        cmd.assert().stdout(predicate::str::contains("longhorn"));
+        cmd.assert().stdout(predicate::str::contains("longhorns"));
     }
 }
